b9fb335247a8b577bef98330c0358195d1518b5f
Jiang <c5247693@c02pxbsrg8wn.dhcp.ctul.sap.corp>
2018-11-14 16:56:37 +0800

    init

---
+++ app/src/main/java/com/example/demo/photogallery/PhotoGalleryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.demo.photogallery
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoGalleryActivity : BaseFragmentActivity() {
+
+   override fun createFragment():Fragment{
+
+   }
+}
